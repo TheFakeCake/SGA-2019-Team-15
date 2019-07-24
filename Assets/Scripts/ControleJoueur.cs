71b1dd82f34dd82f1fcfbc9d26cfc245a881a214
TheFakeCake <TheFakeCake@users.noreply.github.com>
2019-07-24 16:21:38 +0200

    Ajoute le sous-marin du joueur

---
+++ Assets/Scripts/ControleJoueur.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleJoueur : MonoBehaviour
+{
+    public float forceMoteur = 30;
+    public float forceAcceleration = 20;
+    public float cooldownAcceleration = 1;
+
+    private Rigidbody2D rigidBody2D;
+
+    private float lastDashTime = 0;
+    private bool currentOrientation = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Applique la force de déplacement du sous-marin
+        Vector2 force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rigidBody2D.AddForce(force * forceMoteur);
+
+        // Applique la force d'accélération (dash)
+        if (Input.GetButtonDown("Dash") && canDash()) {
+            rigidBody2D.AddForce(force.normalized * forceAcceleration, ForceMode2D.Impulse);
+            lastDashTime = Time.time;
+        }
+
+        // Définit l'orientation horizontale du sous-marin
+        if (force.x > 0) {
+            setOrientation(true);
+        } 
+        else if (force.x < 0) {
+            setOrientation(false);
+        }
+    }
+
+    private bool canDash()
+    {
+        return (Time.time >= lastDashTime + cooldownAcceleration);
+    }
+
+    private void setOrientation(bool orientation)
+    {
+        if (currentOrientation != orientation) {
+            transform.Rotate(new Vector3(0, 180, 0));
+            currentOrientation = orientation;
+        }
+    }
+}
