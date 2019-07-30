29647af03ed3ce777219985ae4f61991b2fac0c7
TheFakeCake <TheFakeCake@users.noreply.github.com>
2019-07-30 13:43:40 +0200

    Retire le cursor de la souris

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -8,7 +8,7 @@ public class StartButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
